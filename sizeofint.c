d6b0bb8dd7f2edc7853ac669ed5f695eb5b6f286
Aiman Kazi <aiman@vlitservices.com>
2018-06-05 00:49:37 +0530

    understanding the size of int

---
+++ sizeofint.c
@@ -0,0 +1,34 @@
+/*
+ * Filename: sizeofint.c
+ * Path: cprograms
+ * Created Date: Tuesday, June 5th 2018, 12:42:20 am
+ * Author: Aiman Kazi
+ * 
+ * Copyright (c) 2018 Visual Labs Learning Solution
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int arr[5] = {5,1,7,3,9};
+
+   printf("Size of integer in this compiler is %u\n", sizeof(int));
+
+   // Size of integer in this compiler is 4
+
+   
+   for(int i = 0; i < 5; i++)
+   {
+       /* code */
+       printf("Address arr[%d] is %u.\n", i, &arr[i]);
+   }
+
+   /* Address arr[0] is 652004528.
+      Address arr[1] is 652004532.
+      Address arr[2] is 652004536.
+      Address arr[3] is 652004540.
+      Address arr[4] is 652004544.
+   */
+    
+}
